0fd061181a57c2122c6769ae07b553352edd94d4
vgorthi <gorthivani2020@gmail.com>
2023-08-19 19:18:34 +0530

    tf-module updated

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -1,8 +1,19 @@
-vpc={
-  main={
-    cidr= "10.0.0.0/16"
+vpc= {
+  main = {
+    cidr = "10.0.0.0/16"
+
+    subnets = {
+      public = {
+        public1= { cidr =  "10.0.0.0/16",az = "us-east-1a" }
+        public2= { cidr =  "10.0.0.0/16",az = "us-east-1b" }
+      }
+
+    }
+
+
   }
 }
+
 
 
 
